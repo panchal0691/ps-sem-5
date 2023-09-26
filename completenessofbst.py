dd13702b84513c209243276f661060f3d2f11980
Nishant Panchal <120048543+panchal0691@users.noreply.github.com>
2023-09-26 10:21:52 +0530

    Create completenessofbst.py

---
+++ completenessofbst.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        q = deque([root])
+        while q:
+            node = q.popleft()
+            if node:
+                q.append(node.left)
+                q.append(node.right)
+            else:
+                while q:
+                    if q.popleft():
+                        return False
+        return True
+        
